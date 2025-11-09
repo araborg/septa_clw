740f14311a24174ba032a4c94115ebbedf7ddd9b
araborg <arababs2015@gmail.com>
2025-11-09 22:31:50 +0100

    1st assignmt: 2

--- js_assignmt/assign_1/script.js
+++ js_assignmt/assign_1/script.js
@@ -3,4 +3,47 @@ Assignment on JavaScript
 Write 22 Expressions or More Involving the operators 
 such that the answers are provided in the console.
 
+
 */
+
+// + Addition
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+
+const addNum = a + b + c + d;
+console.log(addNum);
+
+// - Subtraction
+const substractNum = d - a;
+console.log(substractNum);
+
+// * Multiplication
+// / Division
+// % Modulus (remainder)
+// ** Exponentiation
+
+// = Assignment
+// += Add and assign
+// -= Subtract and assign
+// *= Multiply and assign
+// /= Divide and assign
+
+// == Equal to
+// === Strictly equal (same value and type)
+
+// != Not equal to
+// !== Strictly not equal
+
+// > Greater than
+// < Less than
+
+// >= Greater than or equal to
+// <= Less than or equal to
+
+//&& Logical AND
+
+// || Logical OR
+
+// ! Logical NOT
