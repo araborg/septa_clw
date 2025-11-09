35fc9df153824eff65ac429f1f1aeb5ece57f0d9
araborg <arababs2015@gmail.com>
2025-11-09 23:07:10 +0100

    2nd assignmt: 11

--- js_assignmt/assign_2/script.js
+++ js_assignmt/assign_2/script.js
@@ -24,6 +24,9 @@ Requirements:
 
 */
 
+/* Store at least 5 students as objects in an 
+    array (each with a name and score). */
+
 const studentList = [
 	{ name: "Kola", score: 60 },
 
@@ -35,3 +38,21 @@ const studentList = [
 
 	{ name: "Salah", score: 70 },
 ];
+
+// map() to get all student names.
+const studentNames = studentList.map((student) => student.name);
+console.log(studentNames); // [ "Kola", "Uche", "Alli", "Godfrey", "Salah" ]
+
+// filter() to find those who passed (score ≥ 50).
+const targetScore = 50;
+const scoresAbove50 = studentList
+	.filter((student) => student.score >= targetScore)
+	.map((sortedStudent) => sortedStudent.name);
+
+console.log(scoresAbove50); // ["Kola", "Uche", "Alli", "Salah" ]
+
+// reduce() to find the average score.
+
+// A for loop to display all student results in the console.
+
+// Use an if...else to assign grades (A, B, C, D, F) based on their score.
