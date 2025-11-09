f099a035feb8c2efebce0a0d2f793e0cef6462f2
araborg <arababs2015@gmail.com>
2025-11-09 22:27:06 +0100

    1st assignmt: 1

--- js_assignmt/assign_1/script.js
+++ js_assignmt/assign_1/script.js
@@ -1,6 +1,6 @@
 /*
 Assignment on JavaScript
-Write 22 Expressions or More Involving the operators such 
-that the answers are provided in the console.
+Write 22 Expressions or More Involving the operators 
+such that the answers are provided in the console.
 
 */
