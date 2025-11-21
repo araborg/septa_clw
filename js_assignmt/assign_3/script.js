9ffdc29645fcad970c2c9ea0b8b4d65cd4ccf2cf
araborg <arababs2015@gmail.com>
2025-11-21 22:53:39 +0100

    udemy_carousel: 9

--- js_assignmt/assign_3/script.js
+++ js_assignmt/assign_3/script.js
@@ -1,5 +1,7 @@
 const items = document.querySelectorAll(".item");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
 const inititalSlide = 0;
 const totalItems = items.length;
-console.log(totalItems);
+// console.log(totalItems);
